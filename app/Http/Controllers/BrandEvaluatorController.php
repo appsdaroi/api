fb1b6c598865af2060a735fcfcdda7a260100e60
Felipe Boni <53157965+felipeboni@users.noreply.github.com>
2023-06-12 13:47:01 -0300

    feat: added avaliador de marcas back-end

---
+++ app/Http/Controllers/BrandEvaluatorController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\BrandEvaluator_User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class BrandEvaluatorController extends Controller
+{
+    public function index()
+    {
+        $users = BrandEvaluator_user::select('brandevaluator_users.id', 'user_id', 'username', 'balance', 'bank', 'brandevaluator_users.created_at', 'brandevaluator_users.updated_at')
+            ->leftJoin('users', function ($join) {
+                $join->on('users.id', '=', 'brandevaluator_users.user_id');
+            })
+            ->get();
+
+        if ($users) {
+            return [
+                "status" => 200,
+                "response" => $users
+            ];
+        }
+
+        return [
+            "status" => 500,
+            "response" => "Erro ao consultar lista de usuários com saldo"
+        ];
+    }
+
+    public function show($user_id)
+    {
+        $user = BrandEvaluator_user::select('user_id', 'balance', 'bank', 'created_at')
+            ->where('user_id', '=', $user_id)
+            ->get();
+
+        if (!$user->isEmpty()) {
+            return [
+                "status" => 200,
+                "response" => [
+                    "user" => $user[0]
+                ]
+            ];
+        }
+
+        return [
+            "status" => 500,
+            "response" => "Usuário não encontrado"
+        ];
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->post(), [
+            "user_id"  => "required",
+            "balance"  => "required",
+            "bank"  => "required",
+        ]);
+
+        if ($validator->fails()) {
+            return response(
+                $validator->errors(),
+                400
+            );
+        }
+
+        $user = BrandEvaluator_user::firstOrCreate(
+            [
+                'user_id' => $request->post()["user_id"]
+            ],
+            [
+                'balance' => $request->post()["balance"],
+                'bank' => $request->post()["bank"],
+            ]
+        );
+
+        return [
+            "status" => 200,
+            "data" => $user
+        ];
+    }
+
+    public function update(Request $request, BrandEvaluator_user $brandevaluator)
+    {
+        $validator = Validator::make($request->all(), [
+            "balance"  => "required",
+        ]);
+
+        if ($validator->fails()) {
+            return response(
+                $validator->errors(),
+                400
+            );
+        }
+
+        $brandevaluator->update([
+            'balance' => $request->all()["balance"],
+        ]);
+
+        return [
+            "status" => 200,
+            "data" => $brandevaluator,
+            "msg" => "Usuário atualizado com sucesso"
+        ];
+    }
+
+    public function destroy(BrandEvaluator_user $brandevaluator)
+    {
+        $brandevaluator->delete();
+
+        return [
+            "status" => 200,
+            "data" => $brandevaluator,
+            "msg" => "Usuário excluído com sucesso"
+        ];
+    }
+}
---
+++ database/migrations/2023_06_12_163929_create_brandevaluator_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brandevaluator_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->unique();
+            $table->integer('balance');
+            $table->string('bank');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brandevaluator_user');
+    }
+};
