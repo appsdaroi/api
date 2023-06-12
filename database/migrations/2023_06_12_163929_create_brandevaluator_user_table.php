6e58b0301fdb77d973f56dc8d19779f971a66866
Felipe Boni <53157965+felipeboni@users.noreply.github.com>
2023-06-12 19:34:51 -0300

    fix: changed model name

--- database/migrations/2023_06_12_163929_create_brandevaluator_user_table.php
+++ database/migrations/2023_06_12_163929_create_brandevaluator_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('BrandEvaluator_users', function (Blueprint $table) {
+        Schema::create('brand_evaluator_users', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->unique();
             $table->integer('balance');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('brandevaluator_user');
+        Schema::dropIfExists('brand_evaluator_users');
     }
 };
