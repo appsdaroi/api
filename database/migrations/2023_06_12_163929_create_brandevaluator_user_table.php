1fc0ddea3bedad4fb771bb502b5c144ce7d50769
Felipe Boni <53157965+felipeboni@users.noreply.github.com>
2023-06-12 19:31:02 -0300

    fix: fixing name typos

--- database/migrations/2023_06_12_163929_create_brandevaluator_user_table.php
+++ database/migrations/2023_06_12_163929_create_brandevaluator_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('brandevaluator_user', function (Blueprint $table) {
+        Schema::create('BrandEvaluator_users', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->unique();
             $table->integer('balance');
