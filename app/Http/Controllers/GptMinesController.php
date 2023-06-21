0654b9a6b7291fdf68147aaf49adb706154d2eb1
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2023-06-21 17:37:06 -0300

    feat: add geracao de casas para gptmines

--- app/Http/Controllers/GptMinesController.php
+++ app/Http/Controllers/GptMinesController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers;
 
 class GptMinesController extends Controller
 {
-    public function login()
+    public function index(): array
     {
-        $login = request('login');
-        $password = request('password');
-        if (!$login || !$password) {
-            return response()->json(['error' => 'Você precisa passar um login e senha válidos!'], 400);
+        $game = [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ];
+        // Generate four random positions
+        $positions = array_rand($game, 4);
+        // Set the corresponding values to 1
+        foreach ($positions as $position) {
+            $subarray = &$game[$position];
+            $subkey = array_rand($subarray);
+            $subarray[$subkey] = 1;
         }
+        return [
+            "status" => 200,
+            "response" => $game
+        ];
     }
 }
