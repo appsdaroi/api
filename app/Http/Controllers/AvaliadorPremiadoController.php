b7f168303e76ceaf27b02515710159d801a32678
Felipe Boni <53157965+felipeboni@users.noreply.github.com>
2023-06-28 06:57:22 -0300

    feat: avaliador premiado v1

--- app/Http/Controllers/AvaliadorPremiadoController.php
+++ app/Http/Controllers/AvaliadorPremiadoController.php
@@ -11,9 +11,9 @@ class AvaliadorPremiadoController extends Controller
 {
     public function index()
     {
-        $users = AvaliadorPremiado::select('avaliador_premiado.id', 'user_id', 'username', 'balance', 'bank', 'avaliador_premiado.created_at', 'avaliador_premiado.updated_at')
+        $users = AvaliadorPremiado::select('avaliador_premiados.id', 'user_id', 'username', 'balance', 'bank', 'avaliador_premiados.created_at', 'avaliador_premiados.updated_at')
             ->leftJoin('users', function ($join) {
-                $join->on('users.id', '=', 'avaliador_premiado.user_id');
+                $join->on('users.id', '=', 'avaliador_premiados.user_id');
             })
             ->get();
 
@@ -82,7 +82,7 @@ class AvaliadorPremiadoController extends Controller
         ];
     }
 
-    public function update(Request $request, AvaliadorPremiado $avaliador_premiado)
+    public function update(Request $request, AvaliadorPremiado $avaliador_premiados)
     {
         $validator = Validator::make($request->all(), [
             "balance"  => "required",
@@ -95,24 +95,24 @@ class AvaliadorPremiadoController extends Controller
             );
         }
 
-        $avaliador_premiado->update([
+        $avaliador_premiados->update([
             'balance' => $request->all()["balance"],
         ]);
 
         return [
             "status" => 200,
-            "data" => $avaliador_premiado,
+            "data" => $avaliador_premiados,
             "msg" => "Usuário atualizado com sucesso"
         ];
     }
 
-    public function destroy(AvaliadorPremiado $avaliador_premiado)
+    public function destroy(AvaliadorPremiado $avaliador_premiados)
     {
-        $avaliador_premiado->delete();
+        $avaliador_premiados->delete();
 
         return [
             "status" => 200,
-            "data" => $avaliador_premiado,
+            "data" => $avaliador_premiados,
             "msg" => "Usuário excluído com sucesso"
         ];
     }
